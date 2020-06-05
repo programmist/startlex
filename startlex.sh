613fb63d9fffe63bc388e23b158ce06ca218278f
Tony Childs <tony.childs@gmail.com>
2020-06-05 11:47:28 -0500

    Factor our repeated code into functions

--- startlex.sh
+++ startlex.sh
@@ -1,6 +1,8 @@
 #!/usr/local/bin/bash
 
-# TODO: Use functions to make more DRY
+#***********************#
+#******* Options *******#
+#***********************#
 
 # Defaults
 APPLICATION="winslow"
@@ -19,6 +21,7 @@ case "${APPLICATION}" in
   *) PORT=6543 ;;
 esac
 
+# TODO: Create command line option for virtual env
 vex_env="3.8.2"
 deus_lex_home="~/Programming/deus_lex"
 
@@ -26,52 +29,74 @@ echo "Application: $APPLICATION:$PORT"
 echo "Target: $TARGET"
 
 
+#**********************#
+#******* Macros *******#
+#**********************#
+
+# Open a new vertical split pane in iTerm2
+function term_vertical_split() {
+  osascript \
+  -e 'tell application "iTerm" to activate' \
+  -e 'tell application "System Events" to tell process "iTerm" to key code 2 using command down'
+}
+
+# Open an new iTerm2 tab
+function term_new_tab() {
+  osascript \
+  -e 'tell application "iTerm" to activate' \
+  -e 'tell application "System Events" to tell process "iTerm" to keystroke "t" using command down'
+}
+
+# Enter virtual environment and change to deus_lex root directory
+function change_dir_and_vex() {
+  osascript \
+  -e 'tell application "System Events" to tell process "iTerm" to keystroke "vex '$vex_env'"' \
+  -e 'tell application "System Events" to tell process "iTerm" to key code 52' \
+  -e 'tell application "System Events" to tell process "iTerm" to keystroke "cd '$deus_lex_home'"' \
+  -e 'tell application "System Events" to tell process "iTerm" to key code 52'
+}
+
+
+#***********************#
+#******* Scripts *******#
+#***********************#
+
 # Create Tunnel to target environment
+osascript -e 'tell application "iTerm" to activate'
+
+change_dir_and_vex
+
 osascript \
--e 'tell application "iTerm" to activate' \
--e 'tell application "System Events" to tell process "iTerm" to keystroke "vex '$vex_env'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
--e 'tell application "System Events" to tell process "iTerm" to keystroke "cd '$deus_lex_home'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
 -e 'tell application "System Events" to tell process "iTerm" to keystroke "gmake 'tunnel-$TARGET'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
+-e 'tell application "System Events" to tell process "iTerm" to key code 52'
 
 
-# Run pserve in new vertical split pane: 
+# Run pserve in new vertical split pane
+term_vertical_split
+change_dir_and_vex
+
 osascript \
--e 'tell application "iTerm" to activate' \
--e 'tell application "System Events" to tell process "iTerm" to key code 2 using command down' \
--e 'tell application "System Events" to tell process "iTerm" to keystroke "vex '$vex_env'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
--e 'tell application "System Events" to tell process "iTerm" to keystroke "cd '$deus_lex_home'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
 -e 'tell application "System Events" to tell process "iTerm" to keystroke "pserve --reload ./'$APPLICATION'/dev-tunnel-all-'$TARGET'.ini"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
+-e 'tell application "System Events" to tell process "iTerm" to key code 52'
 
 
 # Run the application watch server in new vertical split pane
+term_vertical_split
+change_dir_and_vex
+
 osascript \
--e 'tell application "iTerm" to activate' \
--e 'tell application "System Events" to tell process "iTerm" to key code 2 using command down' \
--e 'tell application "System Events" to tell process "iTerm" to keystroke "vex '$vex_env'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
--e 'tell application "System Events" to tell process "iTerm" to keystroke "cd '$deus_lex_home'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
 -e 'tell application "System Events" to tell process "iTerm" to keystroke "gmake '$APPLICATION-frontend-watch'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
+-e 'tell application "System Events" to tell process "iTerm" to key code 52'
 
 
-# Change to working directory using virtualenv and run `git branch` in a new tab
+# Run `git branch` in a new tab
+term_new_tab
+change_dir_and_vex
+
 osascript \
--e 'tell application "iTerm" to activate' \
--e 'tell application "System Events" to tell process "iTerm" to keystroke "t" using command down' \
--e 'tell application "System Events" to tell process "iTerm" to keystroke "vex '$vex_env'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
--e 'tell application "System Events" to tell process "iTerm" to keystroke "cd '$deus_lex_home'"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
 -e 'tell application "System Events" to tell process "iTerm" to keystroke "git branch"' \
--e 'tell application "System Events" to tell process "iTerm" to key code 52' \
+-e 'tell application "System Events" to tell process "iTerm" to key code 52'
 
 
-# Open Winslow in new Chrome tab
+# Open application in new Chrome tab
 open -a "Google Chrome" http://localhost:$PORT
